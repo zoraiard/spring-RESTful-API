710ad76aaccbc952cc2b3bbe1520a8c4d0bc7eb5
zoraiard <zoraiarodrigues@gmail.com>
2019-04-14 18:16:17 -0300

    Initial commit

---
+++ src/main/java/com/zoraiarodrigues/SpringCursoApplication.java
@@ -0,0 +1,13 @@
+package com.zoraiarodrigues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCursoApplication.class, args);
+	}
+
+}
